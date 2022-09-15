e650649f39e98cb4db4f9fcee6986d38a6d26adb
zeroku0 <88263075+zeroku0@users.noreply.github.com>
2022-09-15 11:47:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import os
+from deta import Deta
 from flask import Flask
+from fastapi.responses import StreamingResponse
 
 app = Flask(__name__)
 
+app = FastAPI()
+deta = Deta(os.getenv("Project_Key")) 
+drive = deta.Drive("pics")
+
+'''
 @app.route('/', methods=["GET"])
 def hello_world():
     return "Hello World"
+'''
+
+@app.route('/download/{name}', methods=["GET"])
+def download_img():
+    res = drive.get(name)"
+    return StreamingResponse(res.iter_chunks(1024))
