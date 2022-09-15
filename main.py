dd1ff2ba9dc1e91aaa9dc664bdef57b79221f830
zeroku0 <88263075+zeroku0@users.noreply.github.com>
2022-09-15 12:33:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from fastapi.responses import StreamingResponse
 app = FastAPI()
 drive = Drive("pics")
 
-# @app.route('/', methods=["GET"])
-# def hello_world():
-#     return "Hello World"
-
 @app.get("/{name}")
 def download_img(name: str):
     res = drive.get(name)
-    return StreamingResponse(res)
+    return StreamingResponse(res.iter_chunks(1024))
